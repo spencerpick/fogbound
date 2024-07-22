ea618eb5165310a23ae70a284a4b2adcde90e19e
spencerpick <104776610+spencerpick@users.noreply.github.com>
2024-07-22 19:40:52 +0100

    Made code more modular
    Updated player movement code to make it more modular by using functions.

--- Fogbound/Assets/Scripts/Player/PlayerMovement.cs
+++ Fogbound/Assets/Scripts/Player/PlayerMovement.cs
@@ -11,13 +11,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform cameraTransform;
 
     private CharacterController controller;
+    private float moveX;
+    private float moveZ;
     private Vector3 velocity;
     private float currentSpeed;
     private bool onGround;
 
-    [SerializeField] AudioClip footstepsClip;
-    private AudioSource audioSource;
 
+    private AudioSource audioSource;
     private float footstepTimer; // Timer so we can play footsteps at given intervals
     [SerializeField] private float walkStepInterval = 0.5f; // Interval for walking footsteps
     [SerializeField] private float runStepInterval = 0.3f;  // Interval for running footsteps
@@ -33,14 +34,27 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        HandleMovement();
+
+        HandleJumping();
+
+        PlayFootStepSounds();
+
+
+    }
+
+    private void HandleMovement()
+    {
+        // HANDLE MOVEMENT //
         onGround = controller.isGrounded; //  Store if player is currently on the ground or not
-        if(onGround && velocity.y < 0) // If the player is on the ground and they are not falling
+        if (onGround && velocity.y < 0) // If the player is on the ground and they are not falling
         {
             velocity.y = -2f; // Ensures player stays grounded when they are supposed to be
         }
 
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
+        moveX = Input.GetAxis("Horizontal");
+        moveZ = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * moveX + transform.forward * moveZ; // Vector to store the instructed movement
 
@@ -52,6 +66,12 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move * currentSpeed * Time.deltaTime);
 
+    }
+
+
+    private void HandleJumping()
+    {
+        // HANDLE JUMPING //
         if (onGround && Input.GetKeyDown(KeyCode.Space)) // Space bar is held down and the player is on the floor
         {
             velocity.y = Mathf.Sqrt(jumpingHeight * -2f * gravity); // Use kinematic equation to allow player to jump 
@@ -60,17 +80,24 @@ public class PlayerMovement : MonoBehaviour
         velocity.y += gravity * Time.deltaTime; // Apply gravity to player
         controller.Move(velocity * Time.deltaTime); // Moves the character controller vertically based on velocity after being adjusted for gravity
 
-        if (onGround && (moveX != 0 || moveZ != 0))
-        {
-            footstepTimer -= Time.deltaTime;
 
-            if(Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.LeftShift))
+    }
+
+
+    private void PlayFootStepSounds()
+    {
+        // FOOTSTEP SOUND EFFECTS //
+        if (onGround && (moveX != 0 || moveZ != 0)) // Ensure player is on ground and is moving
+        {
+            footstepTimer -= Time.deltaTime; // Countdown timer
+
+            if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.LeftShift)) // If the player changes between running / walking stop the current sound effect immediately and reset interval timer
             {
                 audioSource.Stop();
                 footstepTimer = 0;
             }
 
-            if (footstepTimer <= 0 && !audioSource.isPlaying)
+            if (footstepTimer <= 0 && !audioSource.isPlaying) // If timer is 0 and there is no footstep sound already playing then play the sound
             {
                 audioSource.pitch = currentSpeed == runSpeed ? 2f : 1f;
                 audioSource.Play();
@@ -78,11 +105,13 @@ public class PlayerMovement : MonoBehaviour
                 footstepTimer = currentSpeed == runSpeed ? runStepInterval : walkStepInterval;
             }
         }
-        else
+        else // The player is not moving or not on the ground so stop playing the footsteps sound effect immediately and reset interval timer
         {
             audioSource.Stop();
             footstepTimer = 0; // Reset the timer when player is no longer moving
         }
-
     }
+
 }
+
+
