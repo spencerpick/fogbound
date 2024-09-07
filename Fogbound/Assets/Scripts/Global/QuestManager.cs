126b7306e7c45796657b175b0fb2042696f2f496
spencerpick <104776610+spencerpick@users.noreply.github.com>
2024-09-07 11:58:11 +0100

    Added thinking sounds and updated quest manager

--- Fogbound/Assets/Scripts/Global/QuestManager.cs
+++ Fogbound/Assets/Scripts/Global/QuestManager.cs
@@ -9,20 +9,39 @@ public class QuestManager : MonoBehaviour
     public TextMeshProUGUI currentThoughtText; // Reference to the text component to be stored in here
     [SerializeField] private Light highlightLightPrefab; // Reference to the prefab of the highlight object to be stored in here
 
-    [SerializeField]
-    private int currentThoughtIndex = 0;
 
-    [SerializeField]
-    private List<string> Thoughts;
+    private List<string> QuestStages;
+    private string currentQuestStage;
+
+    private AudioSource audioSource; // Reference to the audio source
+    private bool thoughtSoundPlayed = false;
 
     private Dictionary<GameObject, Light> activeHighlights = new Dictionary<GameObject, Light>(); // Dictionary will store active highlights in it, i.e. objects currently highlighted (making it easy to remove them)
+
+
+    /// Quest markers ///
+    [SerializeField] private GameObject Orphanage_Entrance_Marker;
+
+
 
     void Start()
     {
 
-        // TODO Add guiding thoughts here
-        Thoughts.Add("Hmm, I should find the orphanage.");
-        Thoughts.Add("");
+        QuestStages = new List<string>();
+        // Initalise the different quest stages
+        QuestStages.Add("Find-Orphanage");
+        QuestStages.Add("Look-Around-Orphanage");
+        QuestStages.Add("Place-Toys");
+        QuestStages.Add("Read-note");
+        QuestStages.Add("Find-House");
+        QuestStages.Add("Reveal-Basement");
+        QuestStages.Add("Complete-Ritual");
+
+        audioSource = GetComponent<AudioSource>();
+        
+        StartCoroutine(DelayQuestStart(2f)); // Delay the initial quest stage update by 2 seconds to avoid sound cut-off
+
+
     }
 
     void OnEnable()
@@ -35,6 +54,44 @@ public class QuestManager : MonoBehaviour
     {
         EventManager.OnThoughtUpdate -= UpdateThought;
         EventManager.OnHighlightObject -= HandleHighlight;
+    }
+
+    void Update()
+    {
+        HandleQuestStage();
+        Debug.Log(currentQuestStage);
+    }
+
+    IEnumerator DelayQuestStart(float delayTime) // Coroutine to delay the quest start and avoid sound cutting off
+    {
+        yield return new WaitForSeconds(delayTime);
+        currentQuestStage = QuestStages[0]; 
+    }
+
+    private void HandleQuestStage()
+    {
+        
+        if(currentQuestStage == "Find-Orphanage")
+        {
+            UpdateThought("Hmm I think I should find the orphanage");
+            if(!thoughtSoundPlayed)
+            {
+                PlayThoughtSound();
+                thoughtSoundPlayed = true;
+            }
+            HandleHighlight(Orphanage_Entrance_Marker.gameObject, 6f, 5f, true, 1.31f);
+        }
+
+
+    }
+
+    private void PlayThoughtSound()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play(); // Use the sound already set in the AudioSource component
+        }
+
     }
 
     public void UpdateThought(string newThought)
@@ -54,8 +111,8 @@ public class QuestManager : MonoBehaviour
                 // Set the local position to (0, 0, 0) so that it spawns at the object's position
                 highlightLight.transform.localPosition = new Vector3(0f, yPosOffset, 0f);
                 highlightLight.color = Color.green;
-                highlightLight.range = 5f;
-                highlightLight.intensity = 3f;
+                highlightLight.range = range;
+                highlightLight.intensity = intensity;
 
                 // Store the reference of the object and the light together in the dictionary to make it easy to get rid of later
                 activeHighlights[obj] = highlightLight;
