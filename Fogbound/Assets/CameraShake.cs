3765b626c9a34c7e072214502d7b5652f9ac5205
Alan Judkins <alan.judkins@gmail.com>
2024-09-08 16:57:09 +0100

    Camera shake and red flash upon life lost
    Audio doesn't work yet

---
+++ Fogbound/Assets/CameraShake.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public Transform cameraTransform; // Reference to the camera's Transform
+    public float shakeDuration = 0.2f; // Duration of the camera shake
+    public float shakeAmount = 0.3f;   // Amount of shake
+    public float decreaseFactor = 1.0f; // Speed at which the shake decreases
+
+    private Vector3 originalPos;
+    private float currentShakeDuration = 0f;
+
+    void Start()
+    {
+        // Store the original position of the camera
+        if (cameraTransform == null)
+        {
+            cameraTransform = GetComponent<Transform>();
+        }
+        originalPos = cameraTransform.localPosition;
+    }
+
+    void Update()
+    {
+        if (currentShakeDuration > 0)
+        {
+            // Apply random shake to the camera's position
+            cameraTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+            // Decrease the shake duration over time
+            currentShakeDuration -= Time.deltaTime * decreaseFactor;
+        }
+        else
+        {
+            // Once the shake is done, reset the camera to its original position
+            currentShakeDuration = 0f;
+            cameraTransform.localPosition = originalPos;
+        }
+    }
+
+    // Method to trigger the camera shake
+    public void TriggerShake()
+    {
+        currentShakeDuration = shakeDuration;
+    }
+}
